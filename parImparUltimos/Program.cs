b6bdc8d2ce6c0929491b9830ae495ac462c74361
Luan-art <luanlf2003@gmail.com>
2024-04-22 14:11:42 -0300

    Add project files.

---
+++ parImparUltimos/Program.cs
@@ -0,0 +1,37 @@
+﻿int []numeros = new int[10];
+
+
+for (int i = 0; i < 10; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine()) ;
+}
+
+Console.WriteLine("São pares: ");
+
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+Console.WriteLine("São impares: ");
+
+for (int i = 0; i < 10; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+Console.WriteLine("Primeiro e último valor: ");
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 0 || i == 9)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
